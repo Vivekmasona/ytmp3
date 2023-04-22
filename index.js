b0e898923b34a98feb3925efa517443971528a17
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-22 23:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ app.get("/audiodl", async (req, res) => {
   try {
     ytdl(url, {
             format: 'mp4',
-            filter: '1280x720',
+            filter: 'videoonly',
             quality: '22'
         }).pipe(res);
 
