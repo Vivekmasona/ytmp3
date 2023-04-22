ea180a307b39824c3cd6bef5335bdb95208a2c07
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-22 06:19:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const ytdl = require("ytdl-core");
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
 const app = express();
-// const fs = require("fs");
+app.use('/static', express.static('./static'));
 
-const corsOptions = {
-  origin: "https://vivekfy.netlify.app", // change this origin as your like
-  // origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-app.use(express.static("./static"));
-const port = process.env.PORT || 5000;
-
-app.get("/", (res) => {
-  res.render("index.html");
+app.listen(3000, () => {
+    console.log("It Works!");
 });
 
-app.get("/get", async (req, res) => {
-  const url = req.query.url;
-  console.log(url);
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-  const thumbnail = info.videoDetails.thumbnails[0].url;
-  let formats = info.formats;
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: './' });
+})
 
-  const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-  // const format = ytdl.chooseFormat(info.formats, { quality: "136" });
-  formats = formats.filter((format) => format.hasAudio === true);
-
-  res.send({ title, thumbnail, audioFormats, formats });
-});
-
-app.get("/download", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, { itag }).pipe(res);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// app.get('*', (req, res) => {
-//   res.render('error')
-// })
-
-app.listen(port, () => {
-  console.log("Running ...");
+app.get('/download', (req, res) => {
+    var url = req.query.url;
+    res.header("Content-Disposition", 'attachment; filename="Video.mp4');
+    ytdl(url, {format: 'mp4'}).pipe(res);
 });
