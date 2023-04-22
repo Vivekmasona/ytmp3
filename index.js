7699240f36dbc40eea8962b246e3274cebf0ccc6
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-22 22:54:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ app.get("/audiodl", async (req, res) => {
     ytdl(url, {
             format: 'mp3',
             filter: 'audioonly',
-            quality: 'mp3'
+            quality: '22'
         }).pipe(res);
 
     } catch (err) {
