f1b33936517617942ce232eb92ddd0e3924de2c7
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-22 22:10:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/hack", async (req, res) => {
   // const format = ytdl.chooseFormat(info.formats, { quality: "249" });
   formats = formats.filter((format) => format.hasAudio === true);
 
-  res.send({ title, thumbnail, audioFormats, videoFormats, formats });
+  res.send({ title, thumbnail, videoFormats, formats });
 });
 
 app.get("/videodl", async (req, res) => {
