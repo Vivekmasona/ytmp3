9f2363dffff4bedeee1e98ed54fbb3f49368d07e
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-23 11:12:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
-require("./config.js").load();
-const fs = require("fs")
-const path = require("path")
-const express = require("express")
-const bodyParser = require("body-parser")
-const chalk = require("chalk")
-const cors = require("cors")
-const session = require("express-session")
-console.clear()
-const jwt = require('jsonwebtoken')
-const Authorise = require("./authorise")
-const app = express();
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-mongoose.connect(`mongodb+srv://adarsh:adarsh@cluster0-swlw4.mongodb.net/notspotify?retryWrites=true&w=majority`,{useUnifiedTopology:true, useNewUrlParser:true})
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
- console.log(chalk.green("[mongoose] connected to atlas server"));
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log("Server running at http://localhost:", PORT);
 });
 
-var whitelist = ['http://localhost:3000', 'http://localhost:4829']
-var corsOptions = {
-  origin: function (origin, callback) {
+app.get("/", (_, res) => {
+  var msg = "hello there";
+  res.json({ status: 200, msg: msg });
+});
 
-      callback(null, true)
+app.get("/mp3", async (req, res, next) => {
+  log("Url: ", req.query.url);
+  try {
+    var url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.status(400).send({
+        status: "failed",
+        message: "Invalid url",
+      });
+    }
+    let title = "audio";
 
-  },credentials: true
-}
-app.use(cors(corsOptions));
+    await ytdl.getBasicInfo(
+      url,
+      {
+        format: "mp4",
+      },
+      (err, info) => {
+        if (err) throw err;
+        title = info.player_response.videoDetails.title.replace(
+          /[^\x00-\x7F]/g,
+          ""
+        );
+      }
+    );
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
-app.use(bodyParser.text());
+    res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+    ytdl(url, {
+      format: "mp3",
+      filter: "audioonly",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status: "failed",
+      message: "An error occured while processing this request.",
+    });
+  }
+});
 
-app.get("/", (req, res) => {
-    res.send("hii")
-})
-
-
-app.get("/host", (req, res) => {
-    res.send(req.headers.host)
-})
-
-
-
-fs.readdir(path.join(__dirname, process.env.routes_dir || "routes"), async function (err, routers) {
-    if (err) throw Error(err.message);
-    for (let r in routers) {
-
-        let route = await require(path.join(__dirname, process.env.routes_dir || "routes", routers[r]))
-        const { root, hook, isProtected = true } = route;
-
-        app.use(...[root, (isProtected === false ? (req, res, next) => next() : Authorise), hook({ hi: "hi" })]);
-        console.log("Attached route " + chalk.yellow("%s") + (isProtected === false ? chalk.red(" [no-auth]") : chalk.green(" [auth]")), root);
-
+app.get("/mp4", async (req, res, next) => {
+  log("Url: ", req.query.url);
+  try {
+    let url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.status(400).send({
+        status: "failed",
+        message: "Invalid url",
+      });
     }
 
+    let title = "video";
 
-    app.listen(process.env.PORT || 4829, () => console.log(`Server listening on port ${process.env.PORT || 4829}`));
+    await ytdl.getBasicInfo(
+      url,
+      {
+        format: "mp4",
+      },
+      (err, info) => {
+        title = info.player_response.videoDetails.title.replace(
+          /[^\x00-\x7F]/g,
+          ""
+        );
+      }
+    );
 
-})
+    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+    ytdl(url, {
+      format: "mp4",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      status: "failed",
+      message: "An error occured while processing this request.",
+    });
+  }
+});
+
+const log = (...msg) => {
+  console.log(msg);
+};
