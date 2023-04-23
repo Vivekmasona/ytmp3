1f5a2c8053e2d68e7f00168ff653bd1ca0eecb10
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-23 21:32:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,10 +57,10 @@ app.get("/mp3", async (req, res) => {
   const itag = req.query.itag;
   const type = req.query.type;
 
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
+  const info = await ytdl.getInfo(url);
+  const title = info.videoDetails.title;
 
-  // res.header("Content-Disposition", `attachment;  filename="vfy_${title}"`);
+  res.header("Content-Disposition", `attachment;  filename="vfy_${title}"`);
   try {
     ytdl(url, {
             format: '128',
