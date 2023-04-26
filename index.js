f6721b2c4dd1337ddaeb162ad9c0f40ac2c9ee32
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-04-26 22:06:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,218 +1,32 @@
-const express = require("express");
-const cors = require("cors");
-const ytdl = require("ytdl-core");
-const app = express();
-// const fs = require("fs");
-const path = require('path')
-const express = require('express')
-const nofavicon = require('express-no-favicons')
-const { yellow, green, gray, blue } = require('chalk')
-const youtube = require('./youtube')
-const downloader = require('./downloader')
-const app = express()
+var http = require('http');
+var fs = require('fs');
+var express = require("express");
+var app = express();
+app.use(express.static('video/'));
 
-const corsOptions = {
-  origin: "https://vivekfy.netlify.app", // change this origin as your like
-  // origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-app.use(express.static("./static"));
-const port = process.env.PORT || 5000;
-
-app.get("/", (res) => {
-  res.render("index.html");
-});
-
-app.get("/hack", async (req, res) => {
-  const url = req.query.url;
-  console.log(url);
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-  const thumbnail = info.videoDetails.thumbnails[0].url;
-  let formats = info.formats;
-
-  const audioFormats = ytdl.filterFormats(info.formats, "audioonly");
-  // const format = ytdl.chooseFormat(info.formats, { quality: "249" });
-  formats = formats.filter((format) => format.hasAudio === true);
-
-  res.send({ title, thumbnail, audioFormats, formats });
-});
-
-app.get("/videodl", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment;  filename="${title}_vivekmasona"`);
-  try {
-    ytdl(url, { itag }).pipe(res);
-  } catch (err) {
-    console.log(err);
-  }
-});
-app.get("/mp3", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'highest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
+list_video=[];
+var fn= fs.readdir('video/', function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
     }
-});
-app.get('/:videoId', (req, res) => {
-    const videoId = req.params.videoId
+        files.forEach(function (file) {
+        list_video.push(file)
+    });
+   });
 
-    try {
-      log(`Streaming ${yellow(videoId)}`)
-      youtube.stream(videoId).pipe(res)
-    } catch (e) {
-      log(e)
-      res.sendStatus(500, e)
-    }
-  });
 
+app.get('/', function(req, res){
+  console.log('connected');  
+  var rand_num= Math.floor(Math.random()*Math.floor(list_video.length));
+  var random_select_file= list_video[rand_num];
+  var readerStream = fs.createReadStream("video/"+random_select_file);
+  readerStream.pipe(res);
+  console.log("playing: "+random_select_file);
   
-app.get("/audio", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
+  
 
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'highest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
-    }
-});
-app.get("/music", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'highest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
-    }
-});
-app.get("/low-audio", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'lowest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
-    }
 });
 
-app.get("/audiodl", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment;  filename="${title}_vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'highest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
-    }
-});
-
-app.get("/low-audiodl", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  // const info = await ytdl.getInfo(url);
-  // const title = info.videoDetails.title;
-
-  res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, {
-            format: 'mp3',
-            filter: 'audioonly',
-            quality: 'lowest'
-        }).pipe(res);
-
-    } catch (err) {
-        console.error(err);
-    }
-});
-
-
-app.get("/video", async (req, res) => {
-  const url = req.query.url;
-  const itag = req.query.itag;
-  const type = req.query.type;
-
-  const info = await ytdl.getInfo(url);
-  const title = info.videoDetails.title;
-
-  // res.header("Content-Disposition", `attachment;  filename="Download from.vivekmasona"`);
-  try {
-    ytdl(url, { itag }).pipe(res);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// app.get('*', (req, res) => {
-//   res.render('error')
-// })
-
-app.listen(port, () => {
-  console.log("Running ...");
+app.listen(3000, (req,res) => {
+  console.log("listening on 3000");
 });
